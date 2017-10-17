8f05376219edff1903586e4ae9eb5a450a038027
NotTheRealSanta <ucsantosh@gmail.com>
2017-10-17 07:51:24 +0530

    added file

---
+++ scheduling_alogos.py
@@ -0,0 +1,10 @@
+
+class Process :
+
+    def __init__ (self, process_id, arrival_time, burst_time, priority) :
+        self.process_id = process_id
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.priority = priority
+    
+
